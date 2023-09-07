7eccb49a4196b3930883d2218bd195fcca1d64c7
Syed-Bipul-Rahman <syedbipulrahman2@gmail.com>
2023-09-08 00:55:55 +0600

    Add: save data to firebase storage

--- app/src/main/java/com/classwork/firebase/MainActivity.java
+++ app/src/main/java/com/classwork/firebase/MainActivity.java
@@ -1,5 +1,6 @@
 package com.classwork.firebase;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,10 +9,13 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.classwork.firebase.databinding.ActivityMainBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
 
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,26 +24,47 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+//initialize firebasedatabase reference
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
 
-
+//save data onlclick litener
         binding.butn.setOnClickListener(v -> {
             String email = binding.email.getText().toString();
             String pass = binding.passweord.getText().toString();
 
             //send data to firebase
 
+            saveData(email, pass);
 
 
-            //sent data to another activity
-
-            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-
-            intent.putExtra("email", email);
-            intent.putExtra("pass", pass);
-
-            startActivity(intent);
             // Toast.makeText(this, email+" "+pass, Toast.LENGTH_SHORT).show();
         });
+
+        binding.authusinggoogle.setOnClickListener(v -> {
+            //sent  to another activity
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+
+
+            startActivity(intent);
+
+        });
+    }
+
+    private void saveData(String email, String pass) {
+
+//getting key
+        String key = databaseReference.push().getKey();
+        Model model = new Model(email, pass);
+        databaseReference.child(key).setValue(model);
+
+        //show alertdialog
+        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Data saved");
+        builder.setMessage("Data saved successfully");
+        builder.setPositiveButton("ok", (dialog, which) -> dialog.dismiss());
+        builder.show();
 
 
     }
---
+++ app/src/main/java/com/classwork/firebase/Model.java
@@ -0,0 +1,27 @@
+package com.classwork.firebase;
+
+public class Model {
+    String email;
+    String pass;
+
+    public Model(String email, String pass) {
+        this.email = email;
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
